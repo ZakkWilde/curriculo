7cede88e97b86f16d4bec9f6f868babb41600fa0
Renan Salazar <renan.salazar@gmail.com>
2020-05-23 01:35:15 +0000

    Table, model, binding, aggregation.

--- webapp/Component.js
+++ webapp/Component.js
@@ -25,6 +25,10 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+			
+			
+			//var oGitHubModel = this.getModel("github");
+			//oGitHubModel.loadData();                         
 		}
 	});
 });
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,8 @@ sap.ui.define([
 	return Controller.extend("ovly.data.binding.curriculo.controller.View1", {
 		onInit: function () {
 			
+			window._panel = this.byId("panel_competencias");
+			
 			var oFonteDeDados = {
 				nome: "Renan",
 				sobrenome: "Salazar",
@@ -23,14 +25,33 @@ sap.ui.define([
 				},
 				quiz_1: 60,
 				quiz_1_media: 3,
-				competencias: ["ABAP", "UI5", "JavaScript"]
+				competencias: ["ABAP", "UI5", "JavaScript"],
+				empresas: [
+					{ name: "E-IT",  cargo: "Desenvolvedor" },
+					{ name: "ROFF",  cargo: "Desenvolvedor" },
+					{ name: "KCSIT", cargo: "Desenvolvedor" }
+			    ]
 			};
 			
-			// var oModeloPessoal = new JSONModel(oFonteDeDados);
-			// console.log(oModeloPessoal);
-			var oModeloPessoal = new JSONModel("https://www.mocky.io/v2/5ec5da323200006400d748f2");
+			 //var oModeloPessoal = new JSONModel(oFonteDeDados);
+			 //console.log(oModeloPessoal);
+			//var oModeloPessoal = new JSONModel("https://www.mocky.io/v2/5ec5da323200006400d748f2");
+			var oModeloPessoal = new JSONModel("https://www.mocky.io/v2/5ec86c232f00001219db6fe7");
 			this.getView().setModel(oModeloPessoal);
+			
+			// var sGitHub = "https://api.github.com/users/ZakkWilde/repos";
+			// var oModeloGitHub = new JSONModel(sGitHub);
+			// this.byId("table_github").setModel(oModeloGitHub, "github");
 
+		},
+		
+		toUpper: function(sTexto){
+			if(!sTexto){
+				return "???";
+			}
+			// if(sTexto){
+			return sTexto.toUpperCase();
+			// }
 		}
 	});
 });
